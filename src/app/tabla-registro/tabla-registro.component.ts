ddd309d9c9e0ff111a57eeac3d7dbb3b9fb29332
Adri <adfepa42@hotmail.com>
2018-09-27 18:07:17 +0200

    cambios practica tabla

--- src/app/tabla-registro/tabla-registro.component.ts
+++ src/app/tabla-registro/tabla-registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-tabla-registro',
   templateUrl: './tabla-registro.component.html',
@@ -7,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaRegistroComponent implements OnInit {
 
+  title: any = 'Tabla de Empleados';
+
+  Employees = [
+    { 'name': 'Alberto', position: 'Manager', email: 'adefepadasad@hotmail.com'},
+    { 'name': 'Adrian', position: 'Developer', email: 'adefepadasad@hotmail.com'},
+    { 'name': 'MArio', position: 'Architect', email: 'adefepadasad@hotmail.com'}
+  ];
+model: any = {};
+addEmployee(): void {
+this.Employees.push(this.model);
+}
+deleteEmployee(): void {
+
+}
+editEmployee(): void {
+
+}
+updateEmployee(): void {
+
+}
   constructor() { }
 
   ngOnInit() {
