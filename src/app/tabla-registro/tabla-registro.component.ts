db60782bb6794483fefa9fcd24ec9d0db4f8283f
Adri <adfepa42@hotmail.com>
2018-09-27 13:52:59 +0200

    nuevo componente

---
+++ src/app/tabla-registro/tabla-registro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-registro',
+  templateUrl: './tabla-registro.component.html',
+  styleUrls: ['./tabla-registro.component.sass']
+})
+export class TablaRegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
